e1710bf423ec776937769f8e7653c97151254a52
Maicon Douglas <mdprogamacao@outlook.com>
2026-01-21 07:17:07 -0300

    apredendo sobremetodos sobrecarregados, construtores, bloco de inicialização

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("BMW",  380);
+        Carro carro2 = new Carro("Audi", 270);
+        Carro carro3 = new Carro("BYD",  480);
+
+        carro1.imprimi();
+        carro2.imprimi();
+        carro3.imprimi();
+
+        Carro.velocidadeLimite = 180;
+
+    }
+}
---
+++ src/numero1.java
@@ -0,0 +1,2 @@
+public class numero1 {
+}
